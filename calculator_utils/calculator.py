01c5d02402bdf0a5827a1227870659970c748273
Chinmay Arankalle <ak.chinmay@gmail.com>
2025-02-03 12:30:53 +0530

    docs: Added pydoc for calculator>Calculator>add method

--- calculator_utils/calculator.py
+++ calculator_utils/calculator.py
@@ -1,5 +1,12 @@
 class Calculator:
     def add(self, a, b):
+        """
+        Method adds the two numbers and returns the sum
+
+        :param a: 1st number
+        :param b: 2nd number
+        :return: sum of the two numbers
+        """
         return a + b
 
     def subtract(self, a, b):
